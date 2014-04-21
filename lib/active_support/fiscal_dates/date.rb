fc5d90a0d0557da53f406054d5cef5bbedad828f
David Nettle <david.nettle@kinesis.org>
2014-04-22 09:09:48 +1000

    Add range and calendar year methods.

--- lib/active_support/fiscal_dates/date.rb
+++ lib/active_support/fiscal_dates/date.rb
@@ -1,14 +1,32 @@
 class Date
-  def self.current_financial_year
+  def self.current_fiscal_year
     today = Date.today
     today.month < 7 ? today.year : today.year + 1
   end
 
-  def financial_year
+  def self.current_fiscal_year_range
+    today = Date.today
+    if today.month < 7
+      Date.new(today.year - 1, 7)..Date.new(today.year, 6, 30)
+    else
+      Date.new(today.year, 7)..Date.new(today.year + 1, 6, 30)
+    end
+  end
+
+  def self.fiscal_year_ranges_between(start_date, end_date)
+    start_date_fiscal_year_start = start_date.fiscal_year_first_day
+    end_date_fiscal_year_start = end_date.fiscal_year_first_day
+
+    (start_date_fiscal_year_start.year..end_date_fiscal_year_start.year).map do |year|
+      Date.new(year, 7).fiscal_year_range
+    end
+  end
+
+  def fiscal_year
     self.month < 7 ? self.year : self.year + 1
   end
 
-  def financial_year_first_day
+  def fiscal_year_first_day
     if self.month < 7
       Date.new(self.year - 1, 7)
     else
@@ -16,11 +34,27 @@ class Date
     end
   end
 
-  def financial_year_last_day
+  def fiscal_year_last_day
     if self.month < 7
       Date.new(self.year, 6).end_of_month
     else
       Date.new(self.year + 1, 6).end_of_month
     end
+  end
+
+  def fiscal_year_range
+    fiscal_year_first_day..fiscal_year_last_day
+  end
+
+  def calendar_year_first_day
+    Date.new(self.year)
+  end
+
+  def calendar_year_last_day
+    Date.new(self.year).end_of_year
+  end
+
+  def calendar_year_range
+    calendar_year_first_day..calendar_year_last_day
   end
 end
--- spec/lib/active_support/fiscal_dates/date_spec.rb
+++ spec/lib/active_support/fiscal_dates/date_spec.rb
@@ -1,48 +1,90 @@
 require "spec_helper"
+require "pry"
+require "pry-debugger"
 
 describe Date do
-  describe ".current_financial_year" do
+  describe ".current_fiscal_year" do
     context "before july" do
       before { allow(Date).to receive(:today) { Date.new(2013, 1) } }
 
-      it "returns the correct financial year" do
-        expect(Date.current_financial_year).to eq 2013
+      it "returns the correct fiscal year" do
+        expect(Date.current_fiscal_year).to eq 2013
       end
     end
 
     context "after july" do
       before { allow(Date).to receive(:today) { Date.new(2013, 8) } }
 
-      it "returns the correct financial year" do
-        expect(Date.current_financial_year).to eq 2014
+      it "returns the correct fiscal year" do
+        expect(Date.current_fiscal_year).to eq 2014
       end
     end
   end
 
-  describe "#financial_year" do
+  describe ".current_fiscal_year_range" do
+    context "before july" do
+      before { allow(Date).to receive(:today) { Date.new(2013, 1) } }
+
+      it "returns the correct fiscal year range" do
+        expect(Date.current_fiscal_year_range).to eq Date.new(2012, 7)..Date.new(2013, 6, 30)
+      end
+    end
+
+    context "after july" do
+      before { allow(Date).to receive(:today) { Date.new(2013, 8) } }
+
+      it "returns the correct fiscal year" do
+        expect(Date.current_fiscal_year_range).to eq Date.new(2013, 7)..Date.new(2014, 6, 30)
+      end
+    end
+  end
+
+
+  describe ".fiscal_year_ranges_between" do
+    let(:start_date) { Date.new(2012, 1) }
+    let(:end_date) { Date.new(2014, 8) }
+
+    it "returns an array" do
+      expect(Date.fiscal_year_ranges_between(start_date, end_date)).to be_an Array
+    end
+
+    it "returns the correct number of elements" do
+      expect(Date.fiscal_year_ranges_between(start_date, end_date).length).to eq 4
+    end
+
+    it "contains ranges" do
+      expect(Date.fiscal_year_ranges_between(start_date, end_date).first).to be_a Range
+    end
+
+    it "contains ranges with the correct dates" do
+      expect(Date.fiscal_year_ranges_between(start_date, end_date).first).to eq Date.new(2011, 7)..Date.new(2012, 6 ,30)
+    end
+  end
+
+  describe "#fiscal_year" do
     context "before july" do
       let(:date) { Date.new(2013, 01) }
 
-      it "returns the correct financial year" do
-        expect(date.financial_year).to eq date.year
+      it "returns the correct fiscal year" do
+        expect(date.fiscal_year).to eq date.year
       end
     end
 
     context "after july" do
       let(:date) { Date.new(2013, 8) }
 
-      it "returns the correct financial year" do
-        expect(date.financial_year).to eq date.year + 1
+      it "returns the correct fiscal year" do
+        expect(date.fiscal_year).to eq date.year + 1
       end
     end
   end
 
-  describe "#financial_year_first_day" do
+  describe "#fiscal_year_first_day" do
     context "before july" do
       let(:date) { Date.new(2013, 01) }
 
       it "returns the correct date" do
-        expect(date.financial_year_first_day).to eq Date.new(2012, 7)
+        expect(date.fiscal_year_first_day).to eq Date.new(2012, 7)
       end
     end
 
@@ -50,17 +92,17 @@ describe Date do
       let(:date) { Date.new(2013, 8) }
 
       it "returns the correct date" do
-        expect(date.financial_year_first_day).to eq Date.new(2013, 7)
+        expect(date.fiscal_year_first_day).to eq Date.new(2013, 7)
       end
     end
   end
 
-  describe "#financial_year_last_day" do
+  describe "#fiscal_year_last_day" do
     context "before july" do
       let(:date) { Date.new(2013, 01) }
 
       it "returns the correct date" do
-        expect(date.financial_year_last_day).to eq Date.new(2013, 6, 30)
+        expect(date.fiscal_year_last_day).to eq Date.new(2013, 6, 30)
       end
     end
 
@@ -68,8 +110,48 @@ describe Date do
       let(:date) { Date.new(2013, 8) }
 
       it "returns the correct date" do
-        expect(date.financial_year_last_day).to eq Date.new(2014, 6, 30)
+        expect(date.fiscal_year_last_day).to eq Date.new(2014, 6, 30)
       end
+    end
+  end
+
+  describe "#fiscal_year_range" do
+    let(:date) { Date.new(2013, 01) }
+
+    it "returns a range" do
+      expect(date.fiscal_year_range).to be_a Range
+    end
+
+    it "returns the correct dates" do
+      expect(date.fiscal_year_range).to eq Date.new(2012, 7)..Date.new(2013, 6, 30)
+    end
+  end
+
+  describe "#calendar_year_first_day" do
+    let(:date) { Date.new(2013, 8) }
+
+    it "returns the correct date" do
+      expect(date.calendar_year_first_day).to eq Date.new(2013, 1, 1)
+    end
+  end
+
+  describe "#calendar_year_last_day" do
+    let(:date) { Date.new(2013, 8) }
+
+    it "returns the correct date" do
+      expect(date.calendar_year_last_day).to eq Date.new(2013, 12, 31)
+    end
+  end
+
+  describe "#calendar_year_range" do
+    let(:date) { Date.new(2013, 8) }
+
+    it "returns a range" do
+      expect(date.calendar_year_range).to be_a Range
+    end
+
+    it "returns the correct dates" do
+      expect(date.calendar_year_range).to eq Date.new(2013, 1)..Date.new(2013, 12, 31)
     end
   end
 end
